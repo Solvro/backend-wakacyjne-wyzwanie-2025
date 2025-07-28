a60d5b95762824eaf1b92bbc9a78e0823d8b31bd
Bartosz Gotowski <bartoszgotowski03@gmail.com>
2025-07-28 19:14:08 +0200

    fix: add solvro config

--- 1. Podstawy API/solution/src/app.controller.ts
+++ 1. Podstawy API/solution/src/app.controller.ts
@@ -1,11 +1,12 @@
-import { Controller, Get, HttpCode } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, HttpCode } from "@nestjs/common";
 
-@Controller('wakacyjne')
+import { AppService } from "./app.service";
+
+@Controller("wakacyjne")
 export class AppController {
   constructor(private readonly appService: AppService) {}
-  
-  @Get('backend')
+
+  @Get("backend")
   @HttpCode(418)
   getHello() {
     return this.appService.getHello();
