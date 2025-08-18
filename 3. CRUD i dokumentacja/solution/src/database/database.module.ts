358148b144998ce1e63498ce60fcb6fe71f6fc72
pb3d <przembilski@gmail.com>
2025-08-19 01:18:40 +0200

    fix: better db handling

--- 3. CRUD i dokumentacja/solution/src/database/database.module.ts
+++ 3. CRUD i dokumentacja/solution/src/database/database.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
-import { DatabaseService } from './database.service';
-import { DatabaseController } from './database.controller';
+import { Module } from "@nestjs/common";
+
+import { DatabaseController } from "./database.controller";
+import { DatabaseService } from "./database.service";
 
 @Module({
   providers: [DatabaseService],
-  controllers: [DatabaseController]
+  controllers: [DatabaseController],
+  exports: [DatabaseService],
 })
 export class DatabaseModule {}
