994fb2e3a81cd63f657f0fc224c9221b4a52e459
pb3d <przembilski@gmail.com>
2025-08-19 01:18:40 +0200

    fix: better db handling

--- 3. CRUD i dokumentacja/solution/src/trip/dto/create-trip.dto.ts
+++ 3. CRUD i dokumentacja/solution/src/trip/dto/create-trip.dto.ts
@@ -3,12 +3,15 @@ import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
 export class CreateTripDto {
   @ApiProperty()
   name: string;
+
   @ApiPropertyOptional()
   plannedBudget?: number;
+
   @ApiPropertyOptional({
     isArray: true,
   })
   expenses?: unknown[];
+  
   @ApiPropertyOptional({
     isArray: true,
   })
