581178ab7e936a11be0b16f6cf318921e9fd3a4c
pb3d <przembilski@gmail.com>
2025-08-18 06:00:51 +0200

    feat: part 3 without full solution

---
+++ 3. CRUD i dokumentacja/solution/src/trip/trip.controller.ts
@@ -0,0 +1,59 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+} from "@nestjs/common";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
+
+import { CreateTripResponseDto } from "./dto/create-trip-reponse.dto";
+import { CreateTripDto } from "./dto/create-trip.dto";
+import { UpdateTripDto } from "./dto/update-trip.dto";
+import { TripService } from "./trip.service";
+
+@Controller("trip")
+@ApiTags("trips")
+export class TripController {
+  constructor(private readonly tripService: TripService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  @ApiOperation({
+    summary: "Create a new trip",
+    description:
+      "Add a trip to which you can supply new expenses and participants",
+  })
+  @ApiResponse({
+    status: 201,
+    description: "Trip created",
+    type: CreateTripResponseDto,
+  })
+  async create(@Body() createTripDto: CreateTripDto) {
+    return this.tripService.create(createTripDto);
+  }
+
+  @Get()
+  async findAll() {
+    return this.tripService.findAll();
+  }
+
+  @Get(":id")
+  async findOne(@Param("id") id: string) {
+    return this.tripService.findOne(+id);
+  }
+
+  @Patch(":id")
+  async update(@Param("id") id: string, @Body() updateTripDto: UpdateTripDto) {
+    return this.tripService.update(+id, updateTripDto);
+  }
+
+  @Delete(":id")
+  async remove(@Param("id") id: string) {
+    return this.tripService.remove(+id);
+  }
+}
