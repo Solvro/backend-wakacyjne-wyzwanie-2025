1972c14ebee47eb93c32517e3740f21b1b153424
Ignacy Smoliński <ignacy.smolinski@gmail.com>
2025-08-21 11:23:35 +0200

    feat: copy of module 3

---
+++ 4. Autoryzacja i walidatory/solution/src/trip/dto/create-trip-reponse.dto.ts
@@ -0,0 +1,28 @@
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
+
+export class CreateTripResponseDto {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  name: string;
+
+  @ApiPropertyOptional()
+  plannedBudget?: number;
+
+  @ApiPropertyOptional({
+    isArray: true,
+  })
+  expenses?: unknown[];
+
+  @ApiPropertyOptional({
+    isArray: true,
+  })
+  participants?: unknown[];
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+}
